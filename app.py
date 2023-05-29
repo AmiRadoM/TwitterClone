a778ade0d91bf60e1e4131331f0f7c89ea73f4dc
AmiRadoM <amirrado10@gmail.com>
2023-05-29 14:20:26 +0300

    adde authentication

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import LoginManager
 
 from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+UPLOAD_PATH = "./static/upload"
 
 migrate = Migrate()
 
@@ -18,25 +20,35 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = 'im secret'
     app.config ['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_PATH
+    app.config['MAX_CONTENT_PATH'] = 6000000
 
-    #initializing flask extentions
+    # initializing flask extentions
     db.init_app(app)
     migrate.init_app(app=app, db=db, render_as_batch=True)
     admin = Admin(app=app, template_mode='bootstrap3')
+    lm = LoginManager(app)
 
-    #adding blueprints to the app
+    # adding blueprints to the app
     from home import homeBP
     from api import apiBP
+    from auth import authBP
     app.register_blueprint(homeBP)
     app.register_blueprint(apiBP)
+    app.register_blueprint(authBP)
 
-    #creating the database (if doesn't exist)
+    # creating the database (if doesn't exist)
     import models
     if not path.exists(DB_NAME):
         db.create_all(app=app)
 
-    #adding the models to the admin
+    # adding the models to the admin
     admin.add_view(ModelView(models.User, db.session))
     admin.add_view(ModelView(models.Tweet, db.session))
+
+    # initiating the LoginManager
+    @lm.user_loader
+    def load_user(user_id):
+        return models.User.query.get(user_id)
     
     return app
---
+++ auth.py
@@ -0,0 +1,82 @@
+# auth.py - all the routes of the authentication methods
+
+from flask import Blueprint, render_template, request, redirect, url_for, Response, flash
+from flask_login import login_user, logout_user, login_required
+
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
+from validate_email import validate_email
+from usernames import is_safe_username
+import os
+
+from models import db, User
+
+authBP = Blueprint('auth', __name__, url_prefix="/auth")
+
+@authBP.route("/login", methods=["GET", "POST"])
+def login():
+    if(request.method == "POST"):
+        data = request.form
+
+        email = data.get("email").lower()
+        password = data.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash(f"Couldn't find account for {email}", "error")
+        elif not check_password_hash(user.password, password):
+            flash("Password is incorrect", "error")           
+        else:
+            login_user(user)
+            return redirect(url_for("home.home"))     
+            
+    return render_template("login.html")
+
+@authBP.route("/signup", methods=["GET", "POST"])
+def signup():
+    if(request.method=="POST"):
+        data = request.form
+
+        username = data.get("username")
+        email = data.get("email").lower()
+        password = data.get("password")
+        f = request.files['pfp']
+        fName = secure_filename(f.filename)
+        fFormat = os.path.splitext(fName)[1]
+
+        if(len(password) < 8):
+            flash("Password needs to be at least 8 characters long", "error")
+        elif(len(password) > 128):
+            flash("Password needs to be less than 128 characters long", "error")
+        elif(len(email) > 320):
+            flash("Email is invalid", "error")
+        elif(not validate_email(email)):
+            flash("Email is invalid", "error")
+        elif(User.query.filter_by(email=email).first() != None):
+            flash("Email is already in use", 'error')
+        elif len(username) > 15 or len(username) < 4:
+            flash("Username length should be 4-15 characters long", "error")
+        elif not is_safe_username(username):
+            flash("Username isn't appropriate (Should include english characters)", "error")
+        elif(User.query.filter_by(username=username).first() != None):
+            flash("Username is already in use", 'error')
+        elif f != None and (fName == '' or fFormat not in ['.png', '.jpg', '.jpeg']):
+            flash("Profile picture needs to be in the following formats: .png, .jpg, .jpeg", 'error')
+        else:
+            newUser = User(username=username, email = email, password=generate_password_hash(password, method='sha256'))
+            db.session.add(newUser)
+            db.session.commit()
+            if(f != None):
+                from app import UPLOAD_PATH
+                f.save(UPLOAD_PATH + f"/pfp/{newUser.id}{fFormat}")
+            
+            login_user(newUser)
+            return redirect(url_for("home.home"))
+
+    return render_template("signup.html")
+
+@authBP.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home.home"))
